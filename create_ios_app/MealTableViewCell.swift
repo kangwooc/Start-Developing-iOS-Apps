61dff8d251337bb6b00f07b7a39da329919595cf
troy choi <troy@akaai.kr>
2020-06-18 15:13:36 +0900

    crate a table view

---
+++ create_ios_app/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  create_ios_app
+//
+//  Created by ella on 2020/06/18.
+//  Copyright © 2020 troy. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
